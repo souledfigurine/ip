ca1c5e408aab73376963a5c545dbb9873450ead0
souledfigurine <freddie1507@gmail.com>
2025-01-23 23:06:42 +0800

    Level-0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Baymax User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Baymax.java
@@ -0,0 +1,12 @@
+public class Baymax {
+    public static void main(String[] args) {
+        String line = "---------------------------------------------------------------\n";
+        System.out.println( line
+                    + "Hello! I'm Baymax, your personal emotional companion :)\n"
+                    + "How may I help you today?\n"
+                    + line
+                    + "Goodbye. Have a nice day, and remember;\n"
+                    + "don't forget to smell the flowers, as you aim for the sky\n"
+                    + line);
+    }
+}
