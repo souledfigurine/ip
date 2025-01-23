04f235fc6cd2eb53b2f47be4d3bd717cedf68610
souledfigurine <freddie1507@gmail.com>
2025-01-23 23:37:14 +0800

    added scanner to level 0

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
 public class Baymax {
     public static void main(String[] args) {
-        String line = "---------------------------------------------------------------\n";
-        System.out.println( line
-                    + "Hello! I'm Baymax, your personal emotional companion :)\n"
-                    + "How may I help you today?\n"
-                    + line
-                    + "Goodbye. Have a nice day, and remember;\n"
-                    + "don't forget to smell the flowers, as you aim for the sky\n"
-                    + line);
+
+        //Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        //Display greeting
+        String line = "______________________________________________________________\n";
+        System.out.println(line
+                + "Hello! I'm Baymax, your personal emotional companion :)\n"
+                + "How may I help you today?\n"
+                + line);
+
+        //Display exit message
+        System.out.println(line
+                + "Goodbye. Have a nice day, and remember;\n"
+                + "don't forget to smell the flowers, as you aim for the sky\n"
+                + line);
+
+        //close scanner
+        scanner.close();
     }
 }
