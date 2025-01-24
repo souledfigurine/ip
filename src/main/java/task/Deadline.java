2899e7b0705532afe49a0dd011d827451f6cc3f7
souledfigurine <freddie1507@gmail.com>
2025-01-24 08:59:01 +0800

    created todo.java, event.java, deadline.java

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,2 +1,13 @@
-package Task;public class Deadline {
+package task;
+
+public class Deadline extends Task{
+    protected String by;
+    public Deadline(String name, String deadline) {
+        super(name);
+        this.by = deadline;
+    }
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
 }
--- src/main/java/task/Todo.java
+++ src/main/java/task/Todo.java
@@ -1,2 +1,11 @@
-package Task;public class Todo {
+package task;
+
+public class Todo extends Task {
+    public Todo(String name) {
+        super(name);
+    }
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
