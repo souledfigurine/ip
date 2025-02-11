dbd9ad72465fc915590b8906366200c982153e08
souledfigurine <freddie1507@gmail.com>
2025-02-11 16:03:49 +0800

    Add LocalDateTime to Deadline.java

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,15 +1,19 @@
 package task;
 
+import java.time.LocalDate;
+
 public class Deadline extends Task{
-    protected String by;
+    protected LocalDate by;
     public Deadline(String name, String deadline) {
         super(name);
-        this.by = deadline;
+        LocalDate time = LocalDate.parse(deadline);
+        this.by = time;
     }
 
     public Deadline(String name, String deadline, Boolean isCompleted) {
         super(name, isCompleted);
-        this.by = deadline;
+        LocalDate time = LocalDate.parse(deadline);
+        this.by = time;
     }
 
     @Override
