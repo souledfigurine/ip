0dc2cf97f2ff55bf67d68756bff789435612991e
souledfigurine <freddie1507@gmail.com>
2025-02-16 20:36:48 +0800

    creats Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -7,60 +7,52 @@ import java.util.Scanner;
 
 public class Ui {
     private Scanner scanner;
+    private static final String LINE = "______________________________________________________________";
 
     public Ui() {
         scanner = new Scanner(System.in);
     }
-
     public static void printNewTask(Task newTask) {
-        System.out.println("______________________________________________________________\n"
-                + "added: " + newTask + "\n"
-                + "______________________________________________________________\n");
+        System.out.print(LINE);
+        System.out.println("added: " + newTask);
+        System.out.print(LINE);
     }
     public static void printNewDeadline(Deadline newTask) {
-        System.out.println("______________________________________________________________\n"
-                + "added: " + newTask + "\n"
-                + "______________________________________________________________\n");
+        System.out.print(LINE);
+        System.out.println("added: " + newTask);
+        System.out.print(LINE);
     }
-
     public static void printNewEvent(Event newTask) {
-        System.out.println("______________________________________________________________\n"
-                + "added: " + newTask + "\n"
-                + "______________________________________________________________\n");
+        System.out.print(LINE);
+        System.out.println("added: " + newTask);
+        System.out.print(LINE);
     }
-
     public static void printMarkAsDone(Task task) {
-        System.out.println("______________________________________________________________");
+        System.out.println(LINE);
         System.out.println("Good job on completing the task!\n" + task);
-        System.out.println("______________________________________________________________");
+        System.out.println(LINE);
     }
-
     public static void printUnmark(Task task) {
-        System.out.println("______________________________________________________________");
+        System.out.println(LINE);
         System.out.println("I have unmarked the task for you!\n" + task);
-        System.out.println("______________________________________________________________");
+        System.out.println(LINE);
     }
-
     public static void printDeleteTask(Task taskToDelete) {
-        System.out.println("______________________________________________________________");
-        System.out.println("yay! 1 less task for you!\n"
-                + taskToDelete );
-        System.out.println("______________________________________________________________");
+        System.out.println(LINE);
+        System.out.println("yay! 1 less task for you!")
+        System.out.println(taskToDelete);
+        System.out.println(LINE);
     }
 
     public void printGreeting() {
-        String line = "______________________________________________________________\n";
-        System.out.println(line
-                + "Hello! I'm Baymax, your personal emotional companion :)\n"
-                + "How may I help you today?\n"
-                + line);
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Baymax, your personal emotional companion :)");
+        System.out.println("How may I help you today?");
+        System.out.println(LINE);
     }
 
 
     public String readCommand(String input) {
         return scanner.nextLine();
     }
-
-
-
 }
